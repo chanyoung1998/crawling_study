827a95d9cd3d2cc04c0ccb793e93cf978168497e
찬영정 <lms062111@naver.com>
2021-06-13 23:17:53 +0900

    naver finance 시가 총액 순으로 코스피,코스닥 상장 종목 정보 불러오기 완성 및 파일 저장

--- 01_crawl.py
+++ 01_crawl.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 def crawl(url):
     data = requests.get(url)
     print(data)
@@ -8,29 +9,51 @@ def crawl(url):
 def getStockInfo(tr):
     tds = tr.findAll("td")
     rank = tds[0].text
-    print(tds)
-    return {"rank":rank}
+    aTag = tds[1].find("a")
+    href = aTag["href"]
+    name = aTag.text
+    nowprice = tds[2].text
+    totalPrice = tds[6].text
+    volume = tds[9].text
+    #print(tds)
+    return {"rank":rank,"name":name,"href":href,"code":href[20:],"nowPrice":nowprice,"totalPrice":totalPrice,"volume":volume}
 
-def parse(pageString):
+def parse(pageString,sosok):
     bsObj = BeautifulSoup(pageString,"html.parser")
     box_type_l = bsObj.find("div",{"class":"box_type_l"})
     type_2 = box_type_l.find("table",{"class":"type_2"})
     t_body = type_2.find("tbody")
     trs = t_body.findAll("tr")
-    tr = trs[1]
-    stock_info = getStockInfo(tr)
-    print(stock_info)
-    return []
+
+    if sosok == 1:
+        sosok = "코스닥"
+    elif sosok == 0:
+        sosok = "코스피"
+
+    stockInfos = []
+    for tr in trs[1:]:
+        try:
+            stock_info = getStockInfo(tr)
+            stock_info["sosok"] = sosok
+            stockInfos.append(stock_info)
+        except Exception as e:
+            #print("error")
+            pass
+
+    return stockInfos
 
 def getSiseMarketSum(sosok,page):
     url = "https://finance.naver.com/sise/sise_market_sum.nhn?sosok={}&page={}".format(sosok, page)
     pageString = crawl(url)
-    list = parse(pageString)
+    list = parse(pageString,sosok)
     return list
 
 result = []
-for page in range(2,3):
-    list = getSiseMarketSum(1,page) # 0코스피,1코스닥
-    result.append(list)
+for page in range(1,2):
+    list = getSiseMarketSum(0,page) # 0코스피,1코스닥
+    result += list
 
 print(result)
+
+file = open("./kospi.json","w+")
+file.write(json.dumps(result))
