ea1b2521147d56dfe245b52f630a7c26e49203df
정찬영 <lms062111@naver.com>
2021-06-22 16:39:13 +0900

    21.6.22

--- stock.py
+++ stock.py
@@ -1,23 +1,39 @@
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
 def crawl(url):
     data = requests.get(url)
     print(data)
     return data.content
 
-def getStockurl(stocks,index,tab):
-    return stocks[tab][index]
+def getCompanyInformation(stocks,index,tab):
+
+    pageString = crawl(stocks[tab][index])
+    bsObj = BeautifulSoup(pageString, "html.parser")
+    wrap = bsObj.find("div", {"id": "wrap"})
+    middle = wrap.find("div", {"id": "middle"})
+    content_wrap = middle.find("div", {"class": "content_wrap"})
+    section_inner_sub = content_wrap.find("div", {"class": "section inner_sub"})
+    coinfo_cp = section_inner_sub.find("iframe", {"id": "coinfo_cp"})
+
+    return coinfo_cp["src"]
 
 # Per
 def getStockPER(url):
-    pageString = crawl(url)
-    bsObj = BeautifulSoup(pageString, "html.parser")
-    wrap = bsObj.find("div", {"id":"wrap"})
-    middle = wrap.find("div",{"id":"middle"})
-    content_wrap = middle.find("div",{"class":"content_wrap"})
-    print(content_wrap)
+    driver = webdriver.Chrome("./chromedriver.exe")
+    driver.implicitly_wait(3)
+    driver.get(url)
+
+    html = driver.page_source
+    soup = BeautifulSoup(html,"html.parser")
+    table = soup.find("table",[{"summary":"주요재무정보를 제공합니다."},{"class":"gHead01 all-width"}])
+
+    print(table)
+
+
+
     PER = 0
     return PER
 
@@ -53,4 +69,4 @@ def news(url):
     pass
 
 stocks = pd.read_csv("./data/kospi_with_newtab.csv")
-print(getStockPER(getStockurl(stocks,0,"종목분석")))
+print(getStockPER(getCompanyInformation(stocks,0,"종목분석")))
